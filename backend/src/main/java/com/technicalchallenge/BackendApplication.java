ca752e1d4aabfd2c651aba37652af0f498a9a6db
Victoria Olusegun <victoriaolusegun@gmail.com>
2025-10-24 18:22:43 +0100

    implementation(enhancement 1): Advanced Trade Search System - Removed the annotations  @EnableJpaRepositories(basePackages = { com.technicalchallenge.repository }) and @EnableTransactionManagement, it was causing unnessary errors whilst testing and the @SpringBootApplication already manages JPA respositories and automatically scans all the components(service, controller etc...)

--- backend/src/main/java/com/technicalchallenge/BackendApplication.java
+++ backend/src/main/java/com/technicalchallenge/BackendApplication.java
@@ -2,11 +2,7 @@ package com.technicalchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableJpaRepositories(basePackages = { "com.technicalchallenge.repository" })
-@EnableTransactionManagement
 @SpringBootApplication
 public class BackendApplication {
 
